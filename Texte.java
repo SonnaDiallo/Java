4de9b700b450cb6caca6b42c5f3de83e09b0d624
Sonna <dyasmine335@gmail.com>
2025-02-18 16:19:36 +0100

    BDD et Java

---
+++ Texte.java
@@ -0,0 +1,6 @@
+public class Texte {
+    public static void main(String[] args){
+        String test = "bonjour bienvenue comment ça va je suis yasmine on apprend ensemble créons un tableau";
+    }
+    
+}
